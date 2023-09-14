2e6087f6eba4db7a43e7bb91d203996c0ff9a4e3
Krishnaaa-29 <acctocode29@gmail.com>
2023-09-14 19:44:35 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Navbar,
+  Footer,
+  CustomerReviews,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
